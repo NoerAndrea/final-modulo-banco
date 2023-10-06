bf114767ca90634a0fec52f5ee07cd3a6a18ed1e
Andrea Noer <andreanoer@gmail.com>
2023-10-05 22:16:25 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+//import userRouter from "./routes/user.routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+//app.use(userRouter);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
+});
+
+app.get('/', (req: Request, res: Response) => {
+    return res
+    .status(200).send({success: true, message:"API - GrowTwitter"})
+});
+
+
