173a77578d5b6d6e3b3d2cf8af00173c723c6ae5
Andrea Noer <andreanoer@gmail.com>
2023-10-26 20:51:13 -0300

    correção de rota no index, para habilitar função delete

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 import { userRoutes } from "./routes/user.routes";
 import { tweetRoutes } from "./routes/tweet.routes";
 import { authRoutes } from "./routes/auth.routes";
+import { retweetRoutes } from "./routes/retweet.router";
+import { likeRoutes } from "./routes/like.router";
+import { followRoutes } from "./routes/follow.router";
 
 dotenv.config();
 
@@ -17,6 +20,9 @@ app.use(cors());
 app.use("/", userRoutes());
 app.use("/auth", authRoutes());
 app.use("/tweet", tweetRoutes());
+app.use("/retweet", retweetRoutes());
+app.use("/like", likeRoutes());
+app.use("/follow", followRoutes());
 
 const port = process.env.PORT;
 
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,38 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import userService from "../services/user.service";
 
-/*async function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    try{
-        const token = req.headers.authorization;
-        
-        if(!token){
-            return res.status(401).send({
-                code:401,
-                message:'Authentication token fail.',
-            });
-        }
-
-        const result = await userService.getByToken(token as string);
-        if(!result.data){
-            return res.status(401).send({
-                code: 401,
-                message: 'Authentication token fail.',
-            });
-        }
-
-        req.body.id = result.data.id;
-
-        next();
-    } catch (error){
-        return res.status(500).send({
-            code: 500,
-            message: error,
-        });
-    }
-}
-
-export default authMiddleware;*/
-
 async function authMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
       const { token } = req.headers;
